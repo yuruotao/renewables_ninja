7f94afb4c05c73248d4fd0c3acc89de1c15db35f
yuruotao <yuruotao@outlook.com>
2023-10-21 17:20:17 +0800

    update

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ logging.basicConfig()
 current_path = str(Path())
 input_data_path = current_path + "/data/model_input"
 lat_lon_path = input_data_path + "/china_mass_center_2019.xlsx"
+solar_path = input_data_path + "/Global-Solar-Power-Tracker.xlsx"
 accounts_path = input_data_path + "/ninja_accounts.xlsx"
 
 # Define the query parameters
@@ -24,20 +25,42 @@ dataset = 'merra2'
 
 
 if __name__ == "__main__":
-    # Import the lat lon data and filter those in guangxi Province
-    coords_df = data_import.province_name_to_lat_lon(lat_lon_path)
-    gx_coords_df = coords_df[coords_df["PAC"].astype(str).str[:2].astype(int) == 45]
-    gx_coords_df = gx_coords_df.reset_index()
-    
-    # Import the accounts and generate clients
-    accounts_df = data_import.ninja_accounts_import(accounts_path)
-    client_list = []
-    for client_iter in range(len(accounts_df)):
-        client = scraper_utils.Client.from_df(str(accounts_df["token"].iloc[client_iter]))
-        client_list.append(client)
-    print(client_list)
-    
 
     
+    # Import the accounts and generate clients
+    #
+    #client_list = []
+    #for client_iter in range(len(accounts_df)):
+    #    client = scraper_utils.Client.from_df(str(accounts_df["token"].iloc[client_iter]))
+    #    client_list.append(client)
+    #print(client_list)
+    
+    solar_df = data_import.solar_power_import(solar_path)
+    solar_df['index'] = range(0, 0+len(solar_df))
+    
+    ### Set general parameters, fixed - These are constant in all processes
+    coord_table = solar_df["index","longitude","latitude"]
+    years = (2022)
+    renewable = "pv"
+    dep_capacity = solar_df["Capacity (MW)", "index"]
+
+    ### Set variable parameters - Include whatever needs to vary over the processes
+    ##################################################################################################
+    # Tokens - 6 tokens/ accounts are needed per process for peak performance
+    account_df = data_import.ninja_accounts_import(accounts_path)
+    token_list = account_df.loc[:,"token"].tolist()
+
+    #################################################################################################
+    
+    ### Parallelize processes over arg_list
+
+    results = scraper_utils.ninja_parallel(coord_table=coord_table,
+                            years= years, 
+                            renewable= renewable, 
+                            capacity_table= dep_capacity)
+    
+    ### Cautionary saving of results
+    results.to_csv("./results/pv_test_south1.csv", index=False)
+    print("Finished scraping.")
     
     
--- utils/data_import.py
+++ utils/data_import.py
@@ -4,13 +4,47 @@ import pandas as pd
 
 
 def province_name_to_lat_lon(xlsx_path):
+    """Get the longitude and latitude from xlsx
+
+    Args:
+        xlsx_path (string): the xlsx file path pointing at the mass centers
+
+    Returns:
+        _type_: _description_
+    """
     lat_lon_df = pd.read_excel(xlsx_path)
     
     lat_lon_df = lat_lon_df.rename(columns = {"lon":"LON", "lat":"LAT", "省":"PROVINCE"})
     lat_lon_df = lat_lon_df.astype({'LON': 'float','LAT': 'float', })
     lat_lon_df = lat_lon_df[["PAC", "NAME", "PROVINCE", "LON", "LAT"]]
     
+    # Import the lat lon data and filter those in guangxi Province
+    #coords_df = data_import.province_name_to_lat_lon(lat_lon_path)
+    #gx_coords_df = coords_df[coords_df["PAC"].astype(str).str[:2].astype(int) == 45]
+    #gx_coords_df = gx_coords_df.reset_index()
+    
     return lat_lon_df
+
+def solar_power_import(xlsx_path):
+    """_summary_
+
+    Args:
+        xlsx_path (string): he xlsx file path pointing at the solar projects
+
+    Returns:
+        _type_: _description_
+    """
+    solar_param_df = pd.read_excel(xlsx_path, sheet_name="Data")
+    #solar_param_df_1 = pd.read_excel(xlsx_path, sheet_name="Below Threashold")
+    
+    solar_param_df = solar_param_df[["Country", "State/Province", "Longitude", "Latitude", "Capacity (MW)", "Project Name"]]
+    solar_param_df = solar_param_df[solar_param_df["Country"]=="China"]
+    solar_param_df = solar_param_df[solar_param_df["State/Province"]=="Guangxi"]
+
+    #solar_param_df = solar_param_df.astype({'LON': 'float','LAT': 'float', })
+
+    print(solar_param_df)
+    return solar_param_df
 
 def ninja_accounts_import(xlsx_path):
     account_df = pd.read_excel(xlsx_path)
@@ -21,4 +55,4 @@ def ninja_accounts_import(xlsx_path):
 
 
 if __name__ == "__main__":
-    province_name_to_lat_lon()
+    print("ha")
