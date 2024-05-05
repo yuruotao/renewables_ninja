bb7e4de6ca7d190c30608f4f70bf550d3d391f9e
yuruotao <yuruotao@outlook.com>
2024-05-05 19:42:32 +0800

    a

--- openinfra_data_crawl.py
+++ openinfra_data_crawl.py
@@ -4,8 +4,8 @@ from osgeo import gdal
 import json
 import geopandas as gpd
 import time
-
-
+import osmium
+from shapely.geometry import LineString, Point
 
 def pbf_to_shapefile(input_pbf, output_shapefile):
     # Convert PBF to Shapefile
@@ -19,10 +19,22 @@ def pbf_to_shapefile(input_pbf, output_shapefile):
         #options=options
     )
 
+class OSMHandler(osmium.SimpleHandler):
+    def __init__(self):
+        super(OSMHandler, self).__init__()
+        self.geometries = []
 
+    def way(self, w):
+        coords = [(n.lon, n.lat) for n in w.nodes]
+        geometry = LineString(coords)
+        self.geometries.append(geometry)
+            
+    def node(self, n):
+        geometry = Point(n.lon, n.lat)
+        self.geometries.append(geometry)
 
 if __name__ == "__main__":
-    
+    """
     # Fetch TileJSON data
     tilejson_url = 'https://openinframap.org/map.json'
     response = requests.get(tilejson_url)
@@ -61,4 +73,32 @@ if __name__ == "__main__":
         temp_gpd.set_crs('EPSG:3857', allow_override=True, inplace=True)
         temp_gpd.to_crs('EPSG:4326', inplace=True)
         temp_gpd.to_file(shp_temp_save_path + id + ".shp")
-        
+    """
+
+    input_file = "./data/china.osm"
+    output_file = "./output.osm"
+    filter_tags = ["highway=pipeline", "power=line", "power=tower", "power=substation", 
+                "power=generator", "power=plant", "power=transformer", "power=compensator", 
+                "power=switch", "telecoms=line", "telecoms=mast", "telecoms=data_center", 
+                "man_made=mast", "man_made=petroleum_well", "man_made=petroleum_pipeline", 
+                "man_made=petroleum_site", "man_made=water_pipeline"]
+    
+    #./osmfilter china.osm --keep="highway=pipeline power=tower =substation =generator =plant = transformer =compensator =switch telecoms=line =mast =data_center man_made=mast =petroleum_well =petroleum_pipeline =petroleum_site =water_pipeline" >filtered.osm
+
+
+
+    input_osm_file = "./data/filtered.osm"
+    output_shapefile = "./output.shp"
+
+    handler = OSMHandler()
+    handler.apply_file(input_osm_file)
+
+    # Convert lines to GeoDataFrame
+    gdf_lines = gpd.GeoDataFrame(geometry=handler.lines)
+
+    # Convert points to GeoDataFrame
+    gdf_points = gpd.GeoDataFrame(geometry=handler.points)
+
+    # Save GeoDataFrames to shapefiles
+    gdf_lines.to_file(output_shapefile.replace('.shp', '_lines.shp'))
+    gdf_points.to_file(output_shapefile.replace('.shp', '_points.shp'))
